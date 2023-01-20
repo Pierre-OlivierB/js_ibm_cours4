bed4cd36275c13eaa157a715912a7ff71d7c3a00
unknown <baudinopierreolivier@gmail.com>
2023-01-20 12:44:13 +0100

    begin with date td

--- index.js
+++ index.js
@@ -4,7 +4,51 @@ function firstWord(e) {
   let first = e.split(" ");
   return first[0];
 }
-console.log(firstWord("Hello World"));
+// console.log(firstWord("Hello World"));
 // firstWord("Hello World");
 
+//*Length String
+// var testPrompt = prompt("Ecrire une phrase : ");
+var testPrompt = "toto";
+var testWidth = testPrompt.length;
+var test = testPrompt[0];
+var result = "";
+for (let i = 0; i < testWidth - 1; i++) {
+  if (i == 0) {
+    result = result + test.toUpperCase();
+  } else if (i >= 1) {
+    result += testPrompt[i];
+  }
+}
+// console.log(result);
+//*ou:
+function firstLetterUp(e) {
+  return e.charAt(0).toUpperCase() + e.slice(1, testWidth - 1);
+}
+var display = firstLetterUp(testPrompt);
+// console.log(display);
+
+//*Date
+var date = new Date();
+var localDate = date.toLocaleDateString();
+var month = date.getMonth();
+// console.log(localDate);
+// console.log(date);
+// console.log(month);
+
+var monthTest = date.toLocaleString("fr-FR", { month: "long" });
+console.log(monthTest);
+//! test
+// console.log(testPrompt.length);
+// console.log(test);
 //
+
+// console.log(testPrompt[0]);
+// testPrompt.push(testPrompt[0].toUpperCase());
+// console.log(testPrompt);
+
+// testPrompt = testPrompt.split();
+// console.log(testPrompt);
+
+// const title = document.getElementById("title");
+// console.log(title);
